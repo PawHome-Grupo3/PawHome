caf7c364e5b6f67521fe2d2cbf81698937340b9e
Paulo Sánchez Hernández <paulo.sanchezhernandez@alum.uca.es>
2025-04-10 17:05:12 +0200

    simplificar código idioma

--- src/main/java/com/grupo3/pawHome/config/LocaleConfig.java
+++ src/main/java/com/grupo3/pawHome/config/LocaleConfig.java
@@ -1,27 +1,49 @@
 package com.grupo3.pawHome.config;
 
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
-import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
+import java.time.Duration;
 import java.util.Locale;
 
 @Configuration
-public class LocaleConfig {
+public class LocaleConfig implements WebMvcConfigurer {
+
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource source = new ResourceBundleMessageSource();
+        source.setBasenames("i18n/messages");
+        source.setDefaultEncoding("ISO-8859-1");
+        source.setUseCodeAsDefaultMessage(true);
+        source.setFallbackToSystemLocale(false);
+        return source;
+    }
 
     @Bean
     public LocaleResolver localeResolver() {
-        SessionLocaleResolver slr = new SessionLocaleResolver();
-        slr.setDefaultLocale(new Locale("es"));
-        return slr;
+        CookieLocaleResolver resolver = new CookieLocaleResolver("locale-cookie");
+        resolver.setDefaultLocale(new Locale("es", "ES"));
+        resolver.setCookieMaxAge(Duration.ofDays(180));
+        return resolver;
     }
 
     @Bean
     public LocaleChangeInterceptor localeChangeInterceptor() {
-        LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
-        lci.setParamName("lang");
-        return lci;
+        LocaleChangeInterceptor interceptor = new LocaleChangeInterceptor();
+        interceptor.setParamName("lang");
+        return interceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
     }
 }
+
