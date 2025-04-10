ea9d961a3521737150c051d8957eee305e9ccba9
Paulo Sánchez Hernández <paulo.sanchezhernandez@alum.uca.es>
2025-04-10 19:40:54 +0200

    add mostrarListaCarrito add fragmentProducto

--- src/main/java/com/grupo3/pawHome/controllers/DefaultController.java
+++ src/main/java/com/grupo3/pawHome/controllers/DefaultController.java
@@ -80,6 +80,9 @@ public class DefaultController {
         return "detalleProducto"; // Carga /templates/index.html
     }
 
+    @GetMapping("/listaCarrito")
+    public String mostrarListaCarrito() { return "listaCarrito"; }
+
     // Metodo para mostrar pagina de inicio
     @GetMapping("/tienda")
     public String mostrarTienda()
