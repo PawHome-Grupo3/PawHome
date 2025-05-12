c6fbc06070a8b2b5a2d10fc03823bb0eb6d3a946
Manueltrmr <manueltrmr@gmail.com>
2025-05-12 20:22:31 +0200

    Añadidas entidad factura, FacturaDataLoader y FacturaRepository

---
+++ src/main/java/com/grupo3/pawHome/entities/Factura.java
@@ -0,0 +1,32 @@
+package com.grupo3.pawHome.entities;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Factura {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private long id;
+
+    @Column(nullable = false)
+    private long precio;
+
+    @Lob
+    private String descripcion;
+
+    @Column(nullable = false)
+    private LocalDate fecha;
+}
