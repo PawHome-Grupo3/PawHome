95a3af6162f0d5d557f60ba52ca882054a786920
Paulo Sánchez Hernández <paulo.sanchezhernandez@alum.uca.es>
2025-05-27 22:48:19 +0200

    delete DataLoaders

--- src/main/java/com/grupo3/pawHome/loaders/DataLoader.java
+++ src/main/java/com/grupo3/pawHome/loaders/DataLoader.java
@@ -17,6 +17,7 @@ public class DataLoader implements CommandLineRunner {
     private final ApadrinarRepository apadrinarRepository;
     private final UsuarioRepository usuarioRepository;
 
+
     public DataLoader(AnimalesRepository animalesRepository, ApadrinarRepository apadrinarRepository, UsuarioRepository usuarioRepository) {
         this.animalesRepository = animalesRepository;
         this.apadrinarRepository = apadrinarRepository;
@@ -241,4 +242,26 @@ public class DataLoader implements CommandLineRunner {
                 usuarioRepository.findById(1).get()
         ));
     }
+
+//    private final FacturaRepository facturaRepository;
+//    public FacturaDataLoader(FacturaRepository facturaRepository) {
+//        this.facturaRepository = facturaRepository;
+//    }
+//
+//    @Override
+//    public void run(String... args) throws Exception {
+//
+//        Factura factura1 = new Factura();
+//        factura1.setPrecio(100);
+//        factura1.setDescripcion("Factura de prueba 1");
+//        factura1.setFecha(LocalDate.now());
+//        facturaRepository.save(factura1);
+//
+//        Factura factura2 = new Factura();
+//        factura2.setPrecio(200);
+//        factura2.setDescripcion("Factura de prueba 2");
+//        factura2.setFecha(LocalDate.now());
+//        facturaRepository.save(factura2);
+//
+//    }
 }
