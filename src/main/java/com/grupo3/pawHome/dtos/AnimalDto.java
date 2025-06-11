36d21911759ba5ae5dc62ad6964af763799e3d9c
Paulo Sánchez Hernández <paulo.sanchezhernandez@alum.uca.es>
2025-06-11 20:36:15 +0200

    implementación paginación y filtros en animales añadido fecha Nacimiento en animales

--- src/main/java/com/grupo3/pawHome/dtos/AnimalDto.java
+++ src/main/java/com/grupo3/pawHome/dtos/AnimalDto.java
@@ -1,6 +1,5 @@
-package com.grupo3.pawHome.entities;
+package com.grupo3.pawHome.dtos;
 
-import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,56 +9,26 @@ import java.time.LocalDate;
 
 @Getter
 @Setter
-@Entity
 @AllArgsConstructor
 @NoArgsConstructor
-public class Animal {
-    @Id
-    @GeneratedValue(strategy= GenerationType.IDENTITY)
+public class AnimalDto {
     private Integer id;
-
-    @Column(nullable = false)
     private String nombre;
-
-    @Column(unique = true)
     private String chip;
-
     private float peso;
-
-    @Column(name = "fecha_nacimiento")
     private LocalDate fechaNacimiento;
-
-    @Column(name = "caracter_social")
     private boolean caracterSocial;
-
-    @Column(length = 1000)
     private String descripcion;
-
     private boolean genero;
-
     private boolean origen;
-
-    @Column(nullable = false, columnDefinition = "BOOLEAN DEFAULT FALSE")
     private boolean adoptado;
-
-    @Column(name = "fecha_llegada")
     private LocalDate fechaLlegada;
-
     private boolean paseable;
-
-    @Column(name = "animal_servicio")
     private boolean animalServicio;
-
-    @Column(name = "ruta_img1")
     private String rutaImg1;
-
-    @Column(name = "ruta_img2")
     private String rutaImg2;
-
-    @Column(name = "ruta_img3")
     private String rutaImg3;
-
-    @Column(name = "stripe_product_id")
     private String stripeProductId;
-
+    private String edad;
 }
+
