61e009828c14ccbdf4b60f4182762da40ede00e0
Paulo Sánchez Hernández <paulo.sanchezhernandez@alum.uca.es>
2025-06-12 11:39:51 +0200

    web config para soporta desarrollo

--- src/main/java/com/grupo3/pawHome/config/SecurityConfig.java
+++ src/main/java/com/grupo3/pawHome/config/SecurityConfig.java
@@ -2,7 +2,9 @@ package com.grupo3.pawHome.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -19,7 +21,8 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(AbstractHttpConfigurer::disable) // Deshabilita CSRF si estás en desarrollo
+                .cors(Customizer.withDefaults()) // 💡 habilita soporte para CORS
+                .csrf(AbstractHttpConfigurer::disable)
                 .formLogin(form -> form
                         .loginPage("/login")
                         .failureUrl("/login?error=true")
@@ -27,6 +30,7 @@ public class SecurityConfig {
                         .permitAll()
                 )
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll() // 💡 permite preflight
                         .requestMatchers("/perfil/**").authenticated()
                         .requestMatchers("/product/v1/checkout").permitAll()
                         .anyRequest().permitAll()
