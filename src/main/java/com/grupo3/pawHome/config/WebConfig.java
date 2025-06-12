67cc6f04ca5f382f28d247b1cd011cf9bbf8c2e2
Paulo Sánchez Hernández <paulo.sanchezhernandez@alum.uca.es>
2025-06-12 11:10:22 +0200

    barra filtros con estilo

--- src/main/java/com/grupo3/pawHome/config/WebConfig.java
+++ src/main/java/com/grupo3/pawHome/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080") // Adjust for your frontend URL
+                .allowedOrigins("https://grupo03-desarrollo.serverjava.net/") // Adjust for your frontend URL
                 .allowedMethods("POST", "GET", "PUT", "OPTIONS", "DELETE");
     }
 }
