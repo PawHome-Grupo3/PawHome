f624f101e3a1de51ab3318cf1f3bdada6028d032
Manueltrmr <manueltmiranda.dev@gmail.com>
2025-06-13 20:54:35 +0200

    Creada entidad Rol

---
+++ src/main/java/com/grupo3/pawHome/entities/Rol.java
@@ -0,0 +1,24 @@
+package com.grupo3.pawHome.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "roles")
+public class Rol {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name; // ROLE_USER, ROLE_ADMIN
+
+
+}
