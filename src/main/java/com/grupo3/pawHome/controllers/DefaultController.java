8d365f2c6fe4a5cb74768221434433a64e35fdf7
Paulo Sánchez Hernández <paulo.sanchezhernandez@alum.uca.es>
2025-06-13 16:44:11 +0200

    Retoques visuales página index.html

--- src/main/java/com/grupo3/pawHome/controllers/DefaultController.java
+++ src/main/java/com/grupo3/pawHome/controllers/DefaultController.java
@@ -1,76 +1,17 @@
 package com.grupo3.pawHome.controllers;
 
-
-import com.grupo3.pawHome.services.EntidadHijaService;
-import com.grupo3.pawHome.services.EntidadPadreService;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-/**
- * Controlador encargado de manejar las solicitudes relacionadas con la entidad principal.
 
- * Este controlador utiliza la anotación {@code @Controller} para ser detectado como un componente
- * Spring MVC y maneja solicitudes HTTP. Su objetivo principal es gestionar las operaciones
- * necesarias para mostrar una lista de entidades en la vista correspondiente.
 
- * Anotaciones importantes:
- * - {@code @Controller}: Indica que esta clase se comporta como un controlador Spring MVC.
- * - {@code @PreAuthorize}: Define que el acceso a ciertos métodos esté restringido
- *   según las reglas de autorización establecidas.
-
- * Dependencias:
- * - {@code EntidadPadreRepository}: Interfaz del repositorio que permite interactuar con
- *   la base de datos para operaciones de persistencia y consulta relacionadas con
- *   la entidad padre.
-
- * Métodos principales:
- * - {@code listEntities}: Maneja solicitudes GET a la URL "/entities", recupera los
- *   datos de las entidades desde la base de datos y los pasa al modelo para mostrarlos
- *   en una vista.
- *
- */
 @Controller
 public class DefaultController {
 
-    private final EntidadHijaService entidadHijaService;
-    private final EntidadPadreService entidadPadreService;
-
-    /**
-     * Constructor de la clase DefaultController.
-     * <p>
-     * Inicializa el controlador principal asignando los servicios
-     * utilizados para gestionar las entidades EntidadPadre y EntidadHija.
-     *
-     * @param entidadHijaService  instancia de {@link EntidadHijaService} que proporciona
-     *                            funcionalidades adicionales relacionadas con la entidad EntidadHija.
-     * @param entidadPadreService instancia de {@link EntidadPadreService} que proporciona
-     *                            funcionalidades adicionales relacionadas con la entidad EntidadPadre.
-     */
-    public DefaultController(EntidadHijaService entidadHijaService, EntidadPadreService entidadPadreService) {
-        this.entidadHijaService = entidadHijaService;
-        this.entidadPadreService = entidadPadreService;
-    }
-
-    /**
-     * Método que lista las entidades disponibles y las añade al modelo para ser utilizadas en la vista.
-     * Recupera todas las entidades de un repositorio y las presenta en una vista específica.
-     *
-     * @param model El objeto del modelo que se utiliza para compartir datos entre el backend y la vista.
-     *              Aquí se añade un atributo llamado "entities" con la lista obtenida del repositorio.
-     * @return Una cadena que representa el nombre de la vista ("entitiesList") donde se renderizarán las entidades.
-     */
-    @GetMapping("/entities")
-    public String listEntities(Model model)
-    {
-        model.addAttribute("entidades", entidadHijaService.findAll());
-        return "entidadesHijas"; // View name
-    }
-
     // Metodo para mostrar pagina de inicio
     @GetMapping("")
-    public String mostrarIndex(Model model)
-    {
+    public String mostrarIndex(Model model) {
         model.addAttribute("currentLocale", LocaleContextHolder.getLocale().getDisplayName());
         return "index"; // Carga /templates/index.html
     }
@@ -126,18 +67,17 @@ public class DefaultController {
         return "perfilUsuarioDonaciones";
     }
 
-    // Metodo para mostrar la página de Reseñas
-    @GetMapping("resenas")
-    public String mostrarResenas()
-    {
-        return "Reseñas";
-    }
+//    // Metodo para mostrar la página de Reseñas
+//    @GetMapping("resenas")
+//    public String mostrarResenas()
+//    {
+//        return "resenas";
+//    }
 
     @GetMapping("/eventos")
     public String mostrarEventos(){
         return "eventos";
     }
-
 
     // Metodo para mostrar la página de Colabora
     @GetMapping("/colabora")
@@ -189,46 +129,6 @@ public class DefaultController {
     @GetMapping("/loginPawHome")
     public String mostrarLogin()
     { return "login"; }
-
-    /**
-     * Gestiona las solicitudes GET para obtener y mostrar la lista de entidades protegidas.
-     * Añade las entidades obtenidas del repositorio al modelo para renderizarlas en la vista correspondiente.
-     *
-     * @param model Objeto {@link Model} que se utiliza para pasar datos desde el controlador a la vista.
-     *              Contendrá la lista de entidades recuperadas desde el repositorio.
-     * @return El nombre de la vista "entitiesList" donde se mostrará la lista de entidades.
-     */
-    @GetMapping("/protected")
-    public String protectedList(Model model)
-    {
-        model.addAttribute("entidades", entidadPadreService.findAll());
-        return "entidadesPadre"; // View name
-    }
-
-    /**
-     * Deletes an EntidadHija entity by its ID using the EntidadHijaService.
-     *
-     * @param id The ID of the EntidadHija to delete.
-     * @return A redirect to the "/protected" endpoint after deletion.
-     */
-    @PostMapping("/entidades/deleteHija/{id}")
-    public String deleteEntidadHija(@PathVariable Long id) {
-        entidadHijaService.deleteById(id);
-        return "redirect:/entities";
-    }
-
-    /**
-     * Deletes an EntidadHija entity by its ID using the EntidadHijaService.
-     *
-     * @param id The ID of the EntidadHija to delete.
-     * @return A redirect to the "/protected" endpoint after deletion.
-     */
-    @PostMapping("/entidades/deletePadre/{id}")
-    public String deleteEntidadPadre(@PathVariable Long id) {
-        entidadPadreService.deleteById(id);
-        return "redirect:/entities";
-    }
-
 }
 
 
