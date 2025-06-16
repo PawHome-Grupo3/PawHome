fd11991a0dc729a0c00a20243070117920f57b4b
laura <lauguirez16@gmail.com>
2025-06-16 18:29:20 +0200

    Retoquitos

--- src/main/java/com/grupo3/pawHome/controllers/DefaultController.java
+++ src/main/java/com/grupo3/pawHome/controllers/DefaultController.java
@@ -70,29 +70,6 @@ public class DefaultController {
         return "eventos";
     }
 
-    @GetMapping("/colabora")
-    public String mostrarColabora()
-    {
-        return "Colabora";
-    }
-
-    @GetMapping("/colabora/dona/donarBizum")
-    public String mostrarColaboraDonaDonarBizum()
-    {
-        return "donarBizum";
-    }
-
-    @GetMapping("/colabora/apadrina")
-    public String mostrarColaboraApadrina()
-    {
-        return "Apadrina";
-    }
-
-    @GetMapping("/colabora/Apadrina/formularioApadrina")
-    public String mostrarformularioapadrina() { return "formularioApadrina"; }
-
-    @GetMapping("/colabora/adopta")
-    public String mostrarColaboraAdopta() { return "Adopta"; }
 
     @GetMapping("/finalfeliz")
     public String mostrarFinalesFelices() { return "FinalFeliz"; }
